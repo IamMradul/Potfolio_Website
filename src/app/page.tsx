fef3d5cc9fe50607a2efb4c74e955eae0d7db6d8
IamMradul <mradulg306@gmail.com>
2025-09-14 21:11:27 +0530

    The heading Extracurriculars is hard coded with color white, fixed that issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,9 +204,10 @@ export default function Home() {
           <div className="container mx-auto px-4 md:px-8">
             <div className="flex flex-col items-start justify-center space-y-4 text-left">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-white">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-gray-900 dark:text-white">
                   Extracurriculars
                 </h2>
+
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   Clubs, volunteering, and creative pursuits.
                 </p>
